62adfd9507c4509b93bde2416cf8a5c0425f24ca
Maksym_enKO <ingool.ua@gmail.com>
2023-01-21 00:31:08 +0200

    jokeAPI was added

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ## [Preview Link](https://ingoo1.github.io/joke-teller/)
 
-Work with:
+Used links:
 
 1. [RapidAPI](https://rapidapi.com/)
 2. [Voise RSS](https://www.voicerss.org/sdk/javascript.aspx)
+3. [JokeAPI](https://sv443.net/jokeapi/v2/)
+4. [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
+5. [web.dev - Async-functions](https://web.dev/async-functions/)
--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
 
-function test() {
-  VoiceRSS.speech({
-    key: '61dae9e838314f7bbdd23874bcb56d23',
-    src: 'Hello, world!',
-    hl: 'en-us',
-    v: 'Linda',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false,
-  });
+// function test() {
+//   VoiceRSS.speech({
+//     key: '61dae9e838314f7bbdd23874bcb56d23',
+//     src: 'Hello, world!',
+//     hl: 'en-us',
+//     v: 'Linda',
+//     r: 0,
+//     c: 'mp3',
+//     f: '44khz_16bit_stereo',
+//     ssml: false,
+//   });
+// }
+
+// test();
+
+// Get jokes from Joke API
+async function getJokes() {
+  const apiUrl =
+    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+  let joke = '';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    console.log(joke);
+  } catch (err) {
+    console.log('Woops!', err);
+  }
 }
 
-test();
+getJokes();
